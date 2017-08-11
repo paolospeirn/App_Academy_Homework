a94854f0b3f974caa26c0323a91d950c67deac29
Paolo Speirn <paolo.speirn@gmail.com>
2017-08-11 08:35:29 -0700

    make lru cache

---
+++ w2/w2d5/lru_cache.rb
@@ -0,0 +1,85 @@
+class LRUCache
+
+  attr_reader :contents
+
+   def initialize(capacity = 5)
+     @capacity = capacity
+     @contents = []
+   end
+
+   def count
+     @contents.length
+   end
+
+   def add(el)
+     if el == "!"
+       self.show
+     else
+       @contents.push(el)
+     end
+
+     if self.contents.length > @capacity
+       shorten
+     end
+   end
+
+   # HERE'S HOW THE SOLUTION DEALT WITH ADDING:
+
+  # def add(el)
+  #   if @cache.include?(el)
+  #     @cache.delete(el)
+  #     @cache << el
+  #   elsif count >= @size
+  #     @cache.shift
+  #     @cache << el
+  #   else
+  #     @cache << el
+  #   end
+  # end
+
+   def show
+     p @contents
+   end
+
+   def shorten
+     if @contents.length > @capacity
+         until @contents.length <= @capacity
+           shift
+         end
+     end
+   end
+
+   private
+
+   def shorten
+     if @contents.length > @capacity
+         until @contents.length <= @capacity
+           shift
+         end
+     end
+   end
+
+  #  def used?(el)
+  #   #identify that an element has been called
+  #  end
+
+  #  def reorder(el)
+  #   # reorder elements based on when they've been used
+  #   if used? (el)
+  #     end
+   #
+  #   end
+ end
+
+if __FILE__ == $PROGRAM_NAME
+
+  bob_cache = LRUCache.new(4)
+
+  bob_cache.add("tangled up in blue")
+  bob_cache.add("michael g")
+  bob_cache.add(70)
+
+  @bob_cache.show
+
+
+end
