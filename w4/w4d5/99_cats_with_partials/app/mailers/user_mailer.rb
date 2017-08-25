1e7b55bbaa7e62175ba6d106cfca3d789ac33039
Paolo Speirn <paolo.speirn@gmail.com>
2017-08-25 00:18:51 -0700

    Add partial and mailer to existing 99 cats app

---
+++ w4/w4d5/99_cats_with_partials/app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  default from: 'everybody@appacademy.io'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: user.username, subject: 'Welcome to 99 Cats')
+  end
+
+
+
+end
